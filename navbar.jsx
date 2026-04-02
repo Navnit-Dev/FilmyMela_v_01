8fed0653bc83b0774941b627127b8092548c311e
Navnit Kumar <navnit.devloper@gmail.com>
2026-04-02 17:15:03 +0530

    Update logo URL in navbar.jsx

---
+++ navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src="https://drive.google.com/u/0/drive-viewer/AKGpihap-6l1VQyUWmzW-JcAxdl-YiY-TxL-JxGGD_xE9A1IYHcP4_BcPeHsu8OVqUZpulNl60C0pS4saHAJiE8kw1JbB2qCQMRjlis=s2560?auditContext=forDisplay" alt="Logo" />
+      </div>
+      {/* Other navbar items go here */}
+    </nav>
+  );
+}
+
+export default Navbar;
